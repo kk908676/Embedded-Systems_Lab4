d5f43a49834e4eb8e338bf9edd448d9108b4bb57
kk908676 <kk908676@gmail.com>
2024-05-29 15:49:44 +0800

    First submit

---
+++ Debug/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/projdefs.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/portable.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/list.h
+C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/FreeRTOS.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/projdefs.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/portable.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/An/Desktop/Embedded\ Systems/Lab/Lab4/Lab4/FreeRTOS/include/list.h:
